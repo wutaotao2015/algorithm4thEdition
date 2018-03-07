6cb2784bc1c9e6efe4d98650cfde69432b7b397e
wutaotao2018 <wttwtt2010@163.com>
2018-03-07 17:27:42 +0800

    test

--- src/com/wtt/practice/test/Test.java
+++ src/com/wtt/practice/test/Test.java
@@ -11,17 +11,19 @@ import edu.princeton.cs.algs4.StdRandom;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        int N = 50;
-        double[] a = new double[N];
-        for (int i = 0; i < N; i++)
-            a[i] = StdRandom.random();
-        for (int i = 0; i < N; i++)
-        {
-            double x = 1.0*i/N;
-            double y = a[i]/2.0;
-            double rw = 0.5/N;
-            double rh = a[i]/2.0;
-            StdDraw.filledRectangle(x, y, rw, rh);
-        }
+//        int N = 50;
+//        double[] a = new double[N];
+//        for (int i = 0; i < N; i++)
+//            a[i] = StdRandom.random();
+//        for (int i = 0; i < N; i++)
+//        {
+//            double x = 1.0*i/N;
+//            double y = a[i]/2.0;
+//            double rw = 0.5/N;
+//            double rh = a[i]/2.0;
+//            StdDraw.filledRectangle(x, y, rw, rh);
+//        }
+        System.out.println(Math.abs(-2147483648));
+        System.out.println(1.0/0.0);
     }
 }
