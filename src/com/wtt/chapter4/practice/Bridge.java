6900e34b94e9f7d1745f285b17d000d25582a03c
wutaotao <531618500@qq.com>
2018-04-17 08:11:47 +0800

    桥

--- src/com/wtt/chapter4/practice/Bridge.java
+++ src/com/wtt/chapter4/practice/Bridge.java
@@ -86,11 +86,14 @@ public class Bridge {
         //7:
         //8: 5
         //9: 0
-        Graph G = new Graph(5);
-        G.addEdge(2, 1);
-        G.addEdge(2, 4);
+        Graph G = new Graph(6);
+        G.addEdge(0, 1);
+        G.addEdge(0, 2);
+        G.addEdge(1, 2);
         G.addEdge(2, 3);
         G.addEdge(3, 4);
+        G.addEdge(3, 5);
+        G.addEdge(4, 5);
         StdOut.println(G);
 
         Bridge bridge = new Bridge(G);
