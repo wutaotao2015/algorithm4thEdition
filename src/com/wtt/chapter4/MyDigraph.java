e3b73256815f4bc4e309f6d20d2a999e1a7ba9ce
wutaotao2018 <wttwtt2010@163.com>
2018-04-20 10:28:30 +0800

    有向图的图复制，拓扑排序的入度队列实现

--- src/com/wtt/chapter4/MyDigraph.java
+++ src/com/wtt/chapter4/MyDigraph.java
@@ -1,6 +1,7 @@
 package com.wtt.chapter4;
 
 import com.wtt.chapter1.practice.Bag;
+import edu.princeton.cs.algs4.Stack;
 
 /**
  * 2018/4/17 16:45 add by wutaotao
@@ -20,6 +21,23 @@ public class MyDigraph {
             adj[i] = new Bag<>();
         }
     }
+
+    public MyDigraph(MyDigraph digraph) {
+
+        this(digraph.V());
+        this.E = digraph.E();
+        for (int i = 0; i < digraph.V(); i++) {
+            // 使用栈保证调用adj时顶点的遍历顺序与原对象相同
+            Stack<Integer> stack = new Stack<>();
+            for (Integer v : digraph.adj(i)) {
+                stack.push(v);
+            }
+            for (Integer w : stack) {
+                adj[i].add(w);
+            }
+        }
+    }
+
     public int V() {return V;}
     public int E() {return E;}
     public void addEdge(int v, int w) {
