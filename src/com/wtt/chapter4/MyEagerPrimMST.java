f78ef525c51df57b205cb9b2cf1d6d9a8d2e0549
wutaotao <531618500@qq.com>
2018-04-24 22:31:51 +0800

    Prim算法的即时实现

--- src/com/wtt/chapter4/MyEagerPrimMST.java
+++ src/com/wtt/chapter4/MyEagerPrimMST.java
@@ -2,6 +2,8 @@ package com.wtt.chapter4;
 
 import com.wtt.chapter2.practice.IndexMinPQ;
 
+import java.util.Arrays;
+
 /**
  * 即时版本的prim算法
  *
@@ -10,6 +12,8 @@ import com.wtt.chapter2.practice.IndexMinPQ;
  *  而即时版本中是先进行出队操作，再去进行mark标记操作，从而避免了无效边加入到队列中，同时加入时通过比较权重
  *  （通过顶点作为索引来定位该顶点到树的最小权重，因为随着树的增大，该顶点到树的路径选择变多，从而权重变小的可能增加）
  *  ，只加入最小权重的一条边，减少了队列中的边数，从而提高了队列的操作速度。
+ *
+ *  edgeTo和disTo的第一个元素为null和0.0,代表着顶点0作为树的起始点，不计入具体的对应关系，即这样剩下的V-1个顶点才可以对应MST的V-1条边
  *
  * 2018/4/24 17:04 add by wutaotao
  */
@@ -30,7 +34,75 @@ public class MyEagerPrimMST {
         marked = new boolean[graph.V()];
         minPQ = new IndexMinPQ<>(graph.V());
 
+        distTo[0] = 0.0;
         minPQ.insert(0, 0.0); //顶点0的key为0.0，顶点x的key为x到树的最小权重
+        while (!minPQ.isEmpty()) {
+            visit(graph, minPQ.delMin());
+        }
+    }
+    private void visit(MyEdgeWeightedGraph graph, int v) {
 
+        marked[v] = true;
+        for (MyEdge myEdge : graph.adj(v)) {
+
+            int w = myEdge.other(v);
+            if (marked[w]) continue;
+            double myEdgeWeight = myEdge.weight();
+            if (myEdgeWeight < distTo[w]) {
+                edgeTo[w] = myEdge;
+                distTo[w] = myEdgeWeight;
+                // 判断优先队列中是否已经包含w的索引
+                if (minPQ.contains(w)) minPQ.changeKey(w, myEdgeWeight);
+                else minPQ.insert(w, myEdgeWeight);
+            }
+        }
+    }
+    public Iterable<MyEdge> edges() {
+        return Arrays.asList(edgeTo);
+    }
+    public static void main(String[] args) {
+
+        MyEdgeWeightedGraph myEdgeWeightedGraph = new MyEdgeWeightedGraph(8);
+        MyEdge myEdge = new MyEdge(0, 7, 0.16);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(0, 2, 0.26);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(0, 4, 0.38);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(0, 6, 0.58);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(7, 1, 0.19);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(7, 5, 0.28);
+        myEdgeWeightedGraph.addEdge(myEdge);
+//        myEdge = new MyEdge(7, 2, 0.34);
+//        myEdgeWeightedGraph.addEdge(myEdge);
+//        myEdge = new MyEdge(7, 4, 0.37);
+//        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(1, 3, 0.29);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(1, 5, 0.32);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(1, 2, 0.36);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(2, 3, 0.17);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(2, 6, 0.40);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(3, 6, 0.52);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(4, 5, 0.35);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(4, 6, 0.93);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(7, 2, 0.34);
+        myEdgeWeightedGraph.addEdge(myEdge);
+        myEdge = new MyEdge(7, 4, 0.37);
+        myEdgeWeightedGraph.addEdge(myEdge);
+
+        MyEagerPrimMST mst = new MyEagerPrimMST(myEdgeWeightedGraph);
+        for (MyEdge edge : mst.edges()) {
+            System.out.println(edge);
+        }
     }
 }
